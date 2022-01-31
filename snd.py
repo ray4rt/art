a0046bf9f1209bf9a652853afd5ae8328bb57369
ray4rt <86026143+ray4rt@users.noreply.github.com>
2022-02-01 00:57:39 +0700

    Update and rename index.html to snd.py

---
+++ snd.py
@@ -0,0 +1,9 @@
+<font color="green"><b>SCRAPPER</b><br>
+URL : https://instagram.com/snd.rubyjane<br>
+Debug API : Aian82jUBaayh==<br>
+Full Name : Snd Rubyjane<br>
+UserName : @snd.rubyjane<br>
+Token : AsET4efuDgg8dAQALui09pPAZZZZD==<br>
+Login : 23:39 [New York]<br>
+Phone : +12099216581<br>
+Old Pass : f****************86<br>
