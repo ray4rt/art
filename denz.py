16439a5223c34cceb207621da942e46313f36fbf
ray4rt <86026143+ray4rt@users.noreply.github.com>
2022-09-15 00:52:54 +0700

    Create denz.py

---
+++ denz.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import time
+def denz(z):
+for e in z + "\n":
+sys.stdout.write(e)
+sys.stdout.flush()
+time.sleep(0.05)
+#Delete
+os.system("clear")
+banner = """.../MR.R4YD3N\...""""
+denz("Hallo admin")
