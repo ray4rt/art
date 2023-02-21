ff72bc3be17258d596c8be8a56852f3f6ce20f0f
ray4rt <86026143+ray4rt@users.noreply.github.com>
2023-02-21 20:05:09 +0700

    Update denz.py

--- denz.py
+++ denz.py
@@ -5,8 +5,12 @@ def denz(z):
 for e in z + "/n":
 sys.stdout.write(e)
 sys.stdout.flush()
-time.sleep(0.05)
+time.sleep(5.05)
 #Delete
 os.system("clear")
 banner = """.../MR.R4YD3N\...""""
-denz("Hallo admin")
+denz("Hallo Realme")
+denz("Device Realme")
+denz("IP: 180.29.290.02")
+denz("Status: Locked")
+denz("21-Feb-2023")
